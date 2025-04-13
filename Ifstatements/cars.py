eb80ff828a0bc2f1cde87a53ed0cc31854d60547
Alphahenry <alphahenry2019@gmail.com>
2025-04-13 16:49:24 +0300

    first commit

---
+++ Ifstatements/cars.py
@@ -0,0 +1,10 @@
+#cars in a shop
+
+cars = ["bmw", "v8", "GTR", "porsche", "ferrari"]
+
+for car in cars:
+    if car == "GTR":
+        print("I am definitely owning it")
+        
+    else:
+        print("No I want it")
---
+++ Ifstatements/ifelse.py
@@ -0,0 +1,16 @@
+age = 19
+
+if age == 19:
+    if  age > 18 and age != 0:
+        print("Thats perfect")
+        print("you are qualified to be in line")
+    
+elif age < 18:
+    print("You are not allowed")
+    
+    
+
+  
+    
+else:
+    print("who are you")
