20525493d6848f30790e2c02e17d73f959943ce3
Alphahenry <alphahenry2019@gmail.com>
2025-04-19 22:24:28 +0300

    first commit

---
+++ dictionaries/work.py
@@ -0,0 +1,16 @@
+Employees = {"Tache": "black hat", "hussein": "sec_analyst", "maalim": "pentester"}
+
+for employee, role in Employees.items():
+    print(f"{employee} :{role}")
+    
+
+
+for employee in Employees.values():
+    print(f"The roles are {employee}")
+
+#coding languages
+languages = {"Tache": "C", "Alpha": "Java", "Hussein": "Python", "Maalim": "Ruby"}
+for  name, lang in languages.items():
+    print(f"{name} is proficient in {lang} language")
+    
+    
