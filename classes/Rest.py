9573dbac567b3d04af8f39c528cd411061e1976e
Alphahenry <alphahenry2019@gmail.com>
2025-04-21 22:30:12 +0300

    added icecream child class

--- classes/Rest.py
+++ classes/Rest.py
@@ -15,6 +15,22 @@ class Restaurant:
         
 rest1 = Restaurant("Al-Fatiha", "Arabic")
 print(rest1.describe())
-rest1.open()
 
+#our street branch for kids offering Ice cream
+#our street branch for kids offering Ice cream
+
+class Icecreamstand(Restaurant):
+    def __init__(self, name, cusine):
+        super().__init__(name, cusine)
+        
+        self.flavours = ["strawberry", "caramel", "chocolate", "vanilla"]
+        
+    def offer_flavors(self):
+        print("offers available include: ")
+        for flav in self.flavours:
+            print(flav)
+stand1 = Icecreamstand("opium","ice")
+print(stand1.describe())
+stand1.open()
+stand1.offer_flavors()
                                                                                                                                                                                                                                                                                                                                                                                                 
